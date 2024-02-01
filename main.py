7d8c380bc2b8a56f4607125ebbb12f5c3f53f96b
Thiago Monteiro <tsmonteiro@gmail.com>
2024-02-01 19:29:57 +0000

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from tercen.client import context as ctx
 import numpy as np
+import math
+import polars as pl
 
+
+#http://127.0.0.1:5400/test/w/5409bc1875748e715c48848fd3004e42/ds/b01374e4-0faa-43d1-8e78-a4e349e1e0d2
 tercenCtx = ctx.TercenContext()
 
-df = (
-    tercenCtx
-    .select(['.y', '.ci', '.ri'], df_lib="pandas")
-    .groupby(['.ci','.ri'], as_index=False)
-    .mean()
-    .rename(columns={".y":"mean"})
-    .astype({".ci": np.int32, ".ri": np.int32})
-)
+def transform_and_scale(x, scale):
+    return math.asinh(x/scale)
+
+
+scale = tercenCtx.operator_property('scale', typeFn=int, default=5)
+
+df = tercenCtx.select(['.y', '.ci', '.ri'])
+
+df = df.with_columns(asinh=pl.col(".y") / scale).drop(".y")
 
 df = tercenCtx.add_namespace(df) 
 tercenCtx.save(df)
